688ccc3c60402e919f6d8cc0e90148a035e31da4
jumadimuhammad <getjumadi@gmail.com>
2020-05-12 10:53:41 +0700

    push repo

---
+++ main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jumadimuhammad/backendgo/model"
+	"github.com/labstack/echo"
+)
+
+func app(e *echo.Echo, store model.UserStore) {
+
+	// curl http://localhost:8080/articles
+	e.GET("/users", func(c echo.Context) error {
+		// Process
+		users := store.All()
+
+		// Response
+		return c.JSON(http.StatusOK, users)
+	})
+
+	// curl http://localhost:8080/articles/1
+	e.GET("/users/:id", func(c echo.Context) error {
+		// Given
+		id, _ := strconv.Atoi(c.Param("id"))
+
+		// Process
+		user := store.Find(id)
+
+		// Response
+		return c.JSON(http.StatusOK, user)
+	})
+
+	// curl http://localhost:8080/articles/1
+	e.GET("/users/:role/role", func(c echo.Context) error {
+		// Given
+		role, _ := strconv.Atoi(c.Param("role"))
+
+		// Process
+		user := store.FindRole(role)
+
+		// Response
+		return c.JSON(http.StatusOK, user)
+	})
+
+	e.POST("/users", func(c echo.Context) error {
+		// Given
+		name := c.FormValue("name")
+		address := c.FormValue("address")
+		telp, _ := strconv.Atoi(c.FormValue("telp"))
+		email := c.FormValue("email")
+		password := c.FormValue("password")
+		role := "3"
+
+		//Hashing password
+		hashed := model.Hash(password)
+
+		// Create instabce
+		user, _ := model.CreateUser(name, address, telp, email, hashed, role)
+
+		// Persist
+		store.Save(user)
+
+		// Response
+		return c.JSON(http.StatusOK, user)
+	})
+
+	e.PUT("/users/:id", func(c echo.Context) error {
+		// Given
+		id, _ := strconv.Atoi(c.Param("id"))
+
+		// Process
+		user := store.Find(id)
+		user.Name = c.FormValue("name")
+		user.Address = c.FormValue("address")
+		user.Telp, _ = strconv.Atoi(c.FormValue("telp"))
+		user.Email = c.FormValue("email")
+		password := c.FormValue("password")
+		user.Password = model.Hash(password)
+
+		// Persists
+		store.Update(user)
+
+		// Response
+		return c.JSON(http.StatusOK, user)
+	})
+
+	e.DELETE("/users/:id", func(c echo.Context) error {
+		// Given
+		id, _ := strconv.Atoi(c.Param("id"))
+
+		// Process
+		user := store.Find(id)
+
+		// Remove
+		store.Delete(user)
+
+		// Response
+		return c.JSON(http.StatusOK, user)
+	})
+
+}
+
+func main() {
+
+	var store model.UserStore
+	store = model.NewUserMySQL()
+
+	e := echo.New()
+	app(e, store)
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
---
+++ model/user.go
@@ -0,0 +1,188 @@
+package model
+
+import (
+	"crypto/sha1"
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+)
+
+type UserStore interface {
+	All() []User
+	Save(*User) error
+	Find(int) *User
+	FindRole(int) []User
+	Update(*User) error
+	Delete(user *User) error
+}
+
+type UserStoreMySQL struct {
+	DB *sql.DB
+}
+
+type User struct {
+	ID       int
+	Name     string
+	Address  string
+	Telp     int
+	Email    string
+	Password string
+	Role     string
+	Token    string
+}
+
+func NewUserMySQL() UserStore {
+	dsn := "root:password@tcp(localhost:3308)/db_charity?parseTime=true&clientFoundRows=true"
+	db, err := sql.Open("mysql", dsn)
+
+	if err != nil {
+		panic(err)
+	}
+	return &UserStoreMySQL{DB: db}
+}
+
+func (store *UserStoreMySQL) All() []User {
+	users := []User{}
+	rows, err := store.DB.Query("SELECT * FROM user")
+
+	if err != nil {
+		return users
+	}
+	user := User{}
+	for rows.Next() {
+		rows.Scan(&user.ID, &user.Name, &user.Address, &user.Telp, &user.Email, &user.Password, &user.Role, &user.Token)
+		users = append(users, user)
+	}
+	return users
+}
+
+func CreateUser(name, address string, telp int, email, password, role string) (*User, error) {
+	return &User{
+		Name:     name,
+		Address:  address,
+		Telp:     telp,
+		Email:    email,
+		Password: password,
+		Role:     role,
+	}, nil
+}
+
+func (store *UserStoreMySQL) Save(user *User) error {
+	result, err := store.DB.Exec(`
+		INSERT INTO user(name, address, telp, email, password, role) VALUES(?,?,?,?,?,?)`,
+		user.Name,
+		user.Address,
+		user.Telp,
+		user.Email,
+		user.Password,
+		user.Role,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	_, err = result.RowsAffected()
+
+	if err != nil {
+		return err
+	}
+
+	lastID, err := result.LastInsertId()
+
+	if err != nil {
+		return err
+	}
+	user.ID = int(lastID)
+
+	return nil
+}
+
+func (store *UserStoreMySQL) Find(id int) *User {
+	user := User{}
+
+	err := store.DB.
+		QueryRow(`SELECT * FROM user WHERE id=?`, id).
+		Scan(
+			&user.ID,
+			&user.Name,
+			&user.Address,
+			&user.Telp,
+			&user.Email,
+			&user.Password,
+			&user.Role,
+			&user.Token,
+		)
+
+	if err != nil {
+		log.Fatal(err)
+		return nil
+	}
+
+	return &user
+}
+
+func (store *UserStoreMySQL) FindRole(role int) []User {
+	users := []User{}
+	rows, err := store.DB.Query("SELECT * FROM user WHERE role=?", role)
+
+	if err != nil {
+		return users
+	}
+	user := User{}
+	for rows.Next() {
+		rows.Scan(&user.ID, &user.Name, &user.Address, &user.Telp, &user.Email, &user.Password, &user.Role, &user.Token)
+		users = append(users, user)
+	}
+	return users
+}
+
+func (store *UserStoreMySQL) Update(user *User) error {
+	result, err := store.DB.Exec(`
+		UPDATE user SET name = ?, address = ?, telp = ?, email = ?, password = ? WHERE id =?`,
+		user.Name,
+		user.Address,
+		user.Telp,
+		user.Email,
+		user.Password,
+		user.ID,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	_, err = result.RowsAffected()
+	if err != nil {
+		return err
+	}
+	return nil
+
+}
+
+func (store *UserStoreMySQL) Delete(user *User) error {
+	result, err := store.DB.Exec(`
+	DELETE FROM user WHERE id = ?`,
+		user.ID,
+	)
+	if err != nil {
+		return err
+	}
+
+	_, err = result.RowsAffected()
+	if err != nil {
+		return nil
+	}
+	return nil
+}
+
+func Hash(password string) string {
+	salt := fmt.Sprintf("%d", time.Now().UnixNano())
+	saltedText := fmt.Sprintf("text: '%s', salt: %s", password, salt)
+	sha := sha1.New()
+	sha.Write([]byte(saltedText))
+	encrypted := sha.Sum(nil)
+
+	return fmt.Sprintf("%x", encrypted)
+}
